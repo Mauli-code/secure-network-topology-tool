cfcd7e6cb58968f01e30ef44fcbb6816ad3c09f6
Mauli-code <shrungarmaske37@gmail.com>
2026-09-06 21:58:31 +0530

    Initialize Node.js CLI project

---
+++ src/app.js
@@ -0,0 +1,5 @@
+console.log("=================================");
+console.log(" Secure Network Topology Tool");
+console.log("=================================");
+
+console.log("Application started successfully.");
